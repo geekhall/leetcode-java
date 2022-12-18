1a4d946dfc9dbf1656065aad95150c1bc8bfc6cb
geekhall <174821300@qq.com>
2022-12-18 20:40:25 +0800

    chore: add answers

---
+++ problems/src/main/java/cn/geekhall/problems/p00400/Solution459.java
@@ -0,0 +1,9 @@
+package cn.geekhall.problems.p00400;
+/**
+ * Solution459
+ * 
+ * @author yiny
+ * @date 2022/12/18 20:20
+ */
+public class Solution459 {
+}
---
+++ problems/src/test/java/cn/geekhall/problems/p00400/Solution459Test.java
@@ -0,0 +1,9 @@
+package cn.geekhall.problems.p00400;
+/**
+ * Solution459Test
+ * 
+ * @author yiny
+ * @date 2022/12/18 20:28
+ */
+public class Solution459Test {
+}
