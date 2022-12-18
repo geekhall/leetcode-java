9045de1b1885c76d16c1ddeb27273088ed065e2c
geekhall <174821300@qq.com>
2022-12-18 20:40:37 +0800

    chore: add answers

--- problems/src/main/java/cn/geekhall/problems/p00400/Solution459.java
+++ problems/src/main/java/cn/geekhall/problems/p00400/Solution459.java
@@ -1,9 +1,28 @@
 package cn.geekhall.problems.p00400;
+
 /**
  * Solution459
- * 
+ *
  * @author yiny
  * @date 2022/12/18 20:20
  */
 public class Solution459 {
+    public boolean repeatedSubstringPattern(String s) {
+        int len = s.length();
+        for (int i = 1; i <= len / 2; i++) {
+            if (len % i == 0) {
+                boolean match = true;
+                for (int j = i; j < len; j++) {
+                    if (s.charAt(j) != s.charAt(j - i)) {
+                        match = false;
+                        break;
+                    }
+                }
+                if (match) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 }
--- problems/src/test/java/cn/geekhall/problems/p00400/Solution459Test.java
+++ problems/src/test/java/cn/geekhall/problems/p00400/Solution459Test.java
@@ -1,9 +1,20 @@
 package cn.geekhall.problems.p00400;
+
+import org.junit.Test;
+
 /**
  * Solution459Test
- * 
+ *
  * @author yiny
  * @date 2022/12/18 20:28
  */
 public class Solution459Test {
+
+    @Test
+    public void repeatedSubstringPattern() {
+        Solution459 solution459 = new Solution459();
+        System.out.println(solution459.repeatedSubstringPattern("abab"));
+        System.out.println(solution459.repeatedSubstringPattern("aba"));
+        System.out.println(solution459.repeatedSubstringPattern("abcabcabcabc"));
+    }
 }
