7e63eb1fa0b932a71965e1fafb102ad19fc7b4fb
geekhall <174821300@qq.com>
2022-12-20 21:06:19 +0800

    chore: add answers

---
+++ problems/src/main/java/cn/geekhall/problems/p00000/p00049/Solution00049.java
@@ -0,0 +1,49 @@
+package cn.geekhall.problems.p00000.p00049;
+
+import java.util.List;
+
+/**
+ * ID:    00049
+ * Title: Group Anagrams
+ * Difficulty: Medium
+ * Description: Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+ *
+ * An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+ *
+ * Example 1:
+ *
+ * Input: strs = ["eat","tea","tan","ate","nat","bat"] Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+ *
+ * Example 2:
+ *
+ * Input: strs = [""] Output: [[""]]
+ *
+ * Example 3:
+ *
+ * Input: strs = ["a"] Output: [["a"]]
+ *
+ * Constraints:
+ *
+ * 1 <= strs.length <= 10 4
+ * 0 <= strs[i].length <= 100
+ * strs[i] consists of lowercase English letters.
+ */
+class Solution00049 {
+
+  public List<List<String>> groupAnagrams(String[] strs) {
+    return null;
+  }
+
+
+  public static void test_00049() {
+    Solution00049 solution = new Solution00049();
+    String[] strs = {"eat","tea","tan","ate","nat","bat"};
+    List<List<String>> result = solution.groupAnagrams(strs);
+    System.out.println(result);
+  }
+
+  public static void main(String[] args) {
+    Solution00049.test_00049();
+  }
+}
+
