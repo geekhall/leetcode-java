48ad8fce7df57fe475483f35d5a216cc79f1db14
geekhall <174821300@qq.com>
2022-12-23 10:35:52 +0800

    chore: add answers

--- problems/src/main/java/cn/geekhall/utils/ListNode.java
+++ problems/src/main/java/cn/geekhall/utils/ListNode.java
@@ -9,7 +9,7 @@ public class ListNode {
     public int val;
     public ListNode next;
     ListNode() {}
-    ListNode(int val) { this.val = val; }
+    public ListNode(int val) { this.val = val; }
     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
 
     /**
@@ -23,6 +23,15 @@ public class ListNode {
             current = current.next;
         }
         return head;
+    }
+
+    public void print() {
+        ListNode current = this;
+        while (current != null) {
+            System.out.print(current.val + " -> ");
+            current = current.next;
+        }
+        System.out.println("null");
     }
 }
 
