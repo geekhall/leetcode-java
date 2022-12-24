b2cafd7ffd2f4f03142904a867acf90b29fe8b5f
geekhall <174821300@qq.com>
2022-12-24 19:55:47 +0800

    chore: add answers

---
+++ problems/src/main/java/cn/geekhall/problems/p00200/p00287/Solution00287.java
@@ -0,0 +1,47 @@
+package cn.geekhall.problems.p00200.p00287;
+
+/**
+ * ID:    00287
+ * Title: Find the Duplicate Number
+ * Difficulty: Medium
+ * Description: Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
+ * 
+ * There is only one repeated number in nums, return this repeated number.
+ * 
+ * You must solve the problem without modifying the array nums and uses only constant extra space.
+ * 
+ * Example 1:
+ * 
+ * Input: nums = [1,3,4,2,2] Output: 2
+ * 
+ * Example 2:
+ * 
+ * Input: nums = [3,1,3,4,2] Output: 3
+ * 
+ * Constraints:
+ * 
+ * 1 <= n <= 10 5
+ * nums.length == n + 1
+ * 1 <= nums[i] <= n
+ * All the integers in nums appear only once except for precisely one integer which appears two or more times.
+ * 
+ * Follow up:
+ * 
+ * How can we prove that at least one duplicate number must exist in nums?
+ * Can you solve the problem in linear runtime complexity?
+ */
+class Solution00287 {
+
+  public static void solution(){
+    System.out.println("solution");
+  }
+
+  public static void test_00287() {
+    Solution00287.solution();
+  }
+
+  public static void main(String[] args) {
+    Solution00287.test_00287();
+  }
+}
+
