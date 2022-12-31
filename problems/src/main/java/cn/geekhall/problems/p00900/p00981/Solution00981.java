5d10d08eb727b35324b023735df7a22330fe4ad3
geekhall <174821300@qq.com>
2022-12-31 19:50:33 +0800

    chore: add answer

---
+++ problems/src/main/java/cn/geekhall/problems/p00900/p00981/Solution00981.java
@@ -0,0 +1,78 @@
+package cn.geekhall.problems.p00900.p00981;
+
+import java.util.HashMap;
+
+import javax.print.attribute.HashAttributeSet;
+
+/**
+ * ID:    00981
+ * Title: Time Based Key-Value Store
+ * Difficulty: Medium
+ * Description: Design a time-based key-value data structure that can store multiple values for the same key at different time stamps and retrieve the key's value at a certain timestamp.
+ *
+ * Implement the TimeMap class:
+ *
+ * TimeMap() Initializes the object of the data structure.
+ * void set(String key, String value, int timestamp) Stores the key key with the value value at the given time timestamp.
+ * String get(String key, int timestamp) Returns a value such that set was called previously, with timestamp_prev <= timestamp. If there are multiple such values, it returns the value associated with the largest timestamp_prev. If there are no values, it returns "".
+ *
+ * Example 1:
+ *
+ * Input ["TimeMap", "set", "get", "get", "set", "get", "get"] [[], ["foo", "bar", 1], ["foo", 1], ["foo", 3], ["foo", "bar2", 4], ["foo", 4], ["foo", 5]] Output [null, null, "bar", "bar", null, "bar2", "bar2"] Explanation TimeMap timeMap = new TimeMap(); timeMap.set("foo", "bar", 1); // store the key "foo" and value "bar" along with timestamp = 1. timeMap.get("foo", 1); // return "bar" timeMap.get("foo", 3); // return "bar", since there is no value corresponding to foo at timestamp 3 and timestamp 2, then the only value is at timestamp 1 is "bar". timeMap.set("foo", "bar2", 4); // store the key "foo" and value "bar2" along with timestamp = 4. timeMap.get("foo", 4); // return "bar2" timeMap.get("foo", 5); // return "bar2"
+ *
+ * Constraints:
+ *
+ * 1 <= key.length, value.length <= 100
+ * key and value consist of lowercase English letters and digits.
+ * 1 <= timestamp <= 10 7
+ * All the timestamps timestamp of set are strictly increasing.
+ * At most 2 * 10 5 calls will be made to set and get.
+ */
+class TimeMap {
+
+  HashMap<String, HashMap<Integer, String>> keyTimeMap;
+  public TimeMap() {
+    keyTimeMap = new HashMap<>();
+  }
+
+  public void set(String key, String value, int timestamp) {
+    HashMap<Integer, String> timeValueMap = keyTimeMap.get(key);
+    if (timeValueMap == null) {
+      timeValueMap = new HashMap<>();
+      keyTimeMap.put(key, timeValueMap);
+    }
+    timeValueMap.put(timestamp, value);
+  }
+
+  public String get(String key, int timestamp) {
+    HashMap<Integer, String> timeValueMap = keyTimeMap.get(key);
+    if (timeValueMap == null) {
+      return "";
+    }
+    if (timeValueMap.containsKey(timestamp)) {
+      return timeValueMap.get(timestamp);
+    }
+    int maxTime = 0;
+    for (Integer time : timeValueMap.keySet()) {
+      if (time <= timestamp && time > maxTime) {
+        maxTime = time;
+      }
+    }
+    return timeValueMap.get(maxTime);
+  }
+
+  public static void test_00981() {
+    TimeMap timeMap = new TimeMap();
+    timeMap.set("foo", "bar", 1);
+    System.out.println(timeMap.get("foo", 1));
+    System.out.println(timeMap.get("foo", 3));
+    timeMap.set("foo", "bar2", 4);
+    System.out.println(timeMap.get("foo", 4));
+    System.out.println(timeMap.get("foo", 5));
+  }
+
+  public static void main(String[] args) {
+    TimeMap.test_00981();
+  }
+}
+
