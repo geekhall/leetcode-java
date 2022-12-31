1e7ecfcf234349420108b89cf93b3fbb774d13ee
geekhall <174821300@qq.com>
2022-12-31 17:10:44 +0800

    chore: add answer

--- problems/src/main/java/cn/geekhall/problems/p00400/p00436/Solution00436.java
+++ problems/src/main/java/cn/geekhall/problems/p00400/p00436/Solution00436.java
@@ -1,6 +1,9 @@
 package cn.geekhall.problems.p00400.p00436;
 
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * ID:    00436
@@ -33,26 +36,41 @@ import java.util.Arrays;
  */
 class Solution00436 {
 
-  // wrong answer when [[1,1],[3,4]]
-  public int[] findRightInterval(int[][] intervals) {
-    int[] result = new int[intervals.length];
-    for (int i = 0; i < intervals.length; i++) {
-      int[] interval = intervals[i];
-      int min = Integer.MAX_VALUE;
-      int minIndex = -1;
-      for (int j = 0; j < intervals.length; j++) {
-        if (i == j) {
-          continue;
-        }
-        int[] interval2 = intervals[j];
-        if (interval2[0] >= interval[1] && interval2[0] < min) {
-          min = interval2[0];
-          minIndex = j;
-        }
-      }
-      result[i] = minIndex;
+  public int[] bs(int[][] intervals,int k){
+    int ans[]=null;
+    int i=0,j=intervals.length-1;
+    while(i<=j){
+      int mid=(i+j)/2;
+      if(intervals[mid][0]>=k){
+        ans=intervals[mid];
+        j=mid-1;
+      }else
+        i=mid+1;
     }
-    return result;
+    return ans;
+  }
+  public int[] findRightInterval(int[][] intervals) {
+    if(intervals.length==1)
+      return new int[]{-1};
+    Map<Integer,Integer> map=new HashMap<>();
+    int i=0;
+    for(int[] a:intervals){
+      map.put(a[0],i++);
+    }
+    Arrays.sort(intervals, Comparator.comparingInt(a->a[0]));
+    int n=intervals.length;
+    int ans[]=new int[n];
+
+    for( i=0;i<n;i++){
+      int i_index=map.get(intervals[i][0]);
+      int req[]=bs(intervals,intervals[i][1]);
+      if(req!=null){
+        int index=map.get(req[0]);
+      ans[i_index]=index;
+      }else
+        ans[i_index]=-1;
+    }
+    return ans;
   }
 
   public static void test_00436() {
@@ -66,7 +84,9 @@ class Solution00436 {
     int[][] intervals3 = {{1, 4}, {2, 3}, {3, 4}};
     result = solution.findRightInterval(intervals3);
     System.out.println(Arrays.toString(result));
-
+    int[][] intervals4 = {{1, 1}, {3, 4}};
+    result = solution.findRightInterval(intervals4);
+    System.out.println(Arrays.toString(result));
   }
 
   public static void main(String[] args) {
