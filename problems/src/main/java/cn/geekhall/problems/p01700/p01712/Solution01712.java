40270af3d865a53e08cf3bcc8cc6da0034ca8ad1
geekhall <174821300@qq.com>
2022-12-31 16:58:37 +0800

    chore: add answer

--- problems/src/main/java/cn/geekhall/problems/p01700/p01712/Solution01712.java
+++ problems/src/main/java/cn/geekhall/problems/p01700/p01712/Solution01712.java
@@ -31,40 +31,49 @@ package cn.geekhall.problems.p01700.p01712;
 class Solution01712 {
 
   public int waysToSplit(int[] nums) {
-    int mod = 1000000007;
-    int n = nums.length;
-    int[] preSum = new int[n];
-    preSum[0] = nums[0];
-    for (int i = 1; i < n; i++) {
-      preSum[i] = preSum[i - 1] + nums[i];
+
+    int MOD = 1000000007;
+    int N = nums.length;
+
+    // prefix array
+    int[] A = new int[N];
+    A[0] = nums[0];
+    for (int i = 1; i < N; ++i) A[i] = A[i - 1] + nums[i];
+
+    int res = 0;
+    for (int i = 1; i < N - 1; ++i) {
+      if (A[i - 1] > (A[N - 1] - A[i - 1]) / 2) break;  // early termination
+      int left = helper(A, A[i - 1], i, true);
+      int right = helper(A, A[i - 1], i, false);
+      if (left == -1 || right == -1) continue;  // none is satisfied
+      res = (res + (right - left + 1) % MOD) % MOD;
     }
-    int result = 0;
-    for (int i = 0; i < n - 2; i++) {
-      int left = preSum[i];
-      int right = preSum[n - 1] - preSum[i];
-      int mid = preSum[n - 1] - left - right;
-      if (left > mid || mid > right) {
-        continue;
-      }
-      int leftIndex = i + 1;
-      int rightIndex = n - 1;
-      while (leftIndex < rightIndex) {
-        int midIndex = leftIndex + (rightIndex - leftIndex) / 2;
-        int leftSum = preSum[midIndex - 1] - preSum[i];
-        int rightSum = preSum[n - 1] - preSum[midIndex - 1];
-        if (leftSum > rightSum) {
-          rightIndex = midIndex;
-        } else {
-          leftIndex = midIndex + 1;
-        }
-      }
-      int leftSum = preSum[leftIndex - 1] - preSum[i];
-      int rightSum = preSum[n - 1] - preSum[leftIndex - 1];
-      if (leftSum <= rightSum) {
-        result = (result + (leftIndex - i)) % mod;
+    return res;
+  }
+
+  private int helper(int[] A, int leftSum, int index, boolean searchLeft) {
+
+    int N = A.length;
+    int l = index, r = N - 2;
+    int res = -1;
+
+    while (l <= r) {
+
+      int m = (r - l) / 2 + l;
+      int midSum = A[m] - A[index - 1];
+      int rightSum = A[N - 1] - A[m];
+
+      if (leftSum <= midSum && midSum <= rightSum) {
+        res = m;
+        if (searchLeft) r = m - 1;
+        else l = m + 1;
+      } else if (leftSum > midSum) {  // shrink left
+        l = m + 1;
+      } else {  // shrink right
+        r = m - 1;
       }
     }
-    return result;
+    return res;
   }
 
   public static void test_01712() {
