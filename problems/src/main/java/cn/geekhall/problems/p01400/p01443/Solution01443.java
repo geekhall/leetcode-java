1d4339b88b243dd22162576bb09cc192d15d0b63
geekhall <174821300@qq.com>
2023-01-11 22:38:30 +0800

    chore: add answer

--- problems/src/main/java/cn/geekhall/problems/p01400/p01443/Solution01443.java
+++ problems/src/main/java/cn/geekhall/problems/p01400/p01443/Solution01443.java
@@ -1,5 +1,6 @@
 package cn.geekhall.problems.p01400.p01443;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -35,8 +36,37 @@ import java.util.List;
 class Solution01443 {
 
   public int minTime(int n, int[][] edges, List<Boolean> hasApple) {
-    int result = 0;
-    return result;
+    // Create an adjacency list
+    List<List<Integer>> adj = new ArrayList<>();
+    // Initialize the adjacency list
+    for(int i = 0; i < n; i++)
+      adj.add(new ArrayList<>());
+    // Add edges to the adjacency list
+    for(int[] edge : edges){
+      adj.get(edge[0]).add(edge[1]);
+      adj.get(edge[1]).add(edge[0]);
+    }
+    // Call the helper function
+    return minTimeToCollectApples(0, adj, hasApple, -1);
+  }
+
+  // Helper function
+  int minTimeToCollectApples(int index, List<List<Integer>> adj, List<Boolean> hasApple, int parent){
+    // variable to keep track of total time
+    int total = 0;
+    // for each neighbor of current node
+    for(int nbr : adj.get(index)){
+        // if the neighbor is the parent node, then we skip
+        if (nbr == parent)
+         continue;
+        // we recursively call the helper function for each neighbor
+        total += minTimeToCollectApples(nbr, adj, hasApple, index);
+    }
+    // If current node is not the root (0th) node, and it has an apple or any of its descendant has
+    //  then we need to add 2 time for moving to and from it
+    if( index != 0 && (hasApple.get(index) || total > 0))
+      total += 2;
+    return total;
   }
 
   public static void test_01443() {
