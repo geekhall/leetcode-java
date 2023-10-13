79142572279de62b7e15de7c97fcca29b0672b72
geekhall <174821300@qq.com>
2023-10-13 13:18:29 +0800

    chore: add answer

---
+++ problems/src/main/java/net/geekhour/problems/p00700/p00746/Solution00746.java
@@ -0,0 +1,62 @@
+package net.geekhour.problems.p00700.p00746;
+
+import java.util.*;
+
+/**
+ * ID: 00746
+ * Title: Min Cost Climbing Stairs
+ * Difficulty: Easy
+ * Description: You are given an integer array cost where cost[i] is the cost of
+ * i th step on a staircase. Once you pay the cost, you can either climb one or
+ * two steps.
+ *
+ * You can either start from the step with index 0, or the step with index 1.
+ *
+ * Return the minimum cost to reach the top of the floor.
+ *
+ * Example 1:
+ *
+ * Input: cost = [10, 15,20] Output: 15 Explanation: You will start at index 1.
+ * - Pay 15 and climb two steps to reach the top. The total cost is 15.
+ *
+ * Example 2:
+ *
+ * Input: cost = [ 1,100, 1,1, 1,100, 1, 1,100, 1 ] Output: 6 Explanation: You
+ * will start at index 0. - Pay 1 and climb two steps to reach index 2. - Pay 1
+ * and climb two steps to reach index 4. - Pay 1 and climb two steps to reach
+ * index 6. - Pay 1 and climb one step to reach index 7. - Pay 1 and climb two
+ * steps to reach index 9. - Pay 1 and climb one step to reach the top. The
+ * total cost is 6.
+ *
+ * Constraints:
+ *
+ * 2 <= cost.length <= 1000
+ * 0 <= cost[i] <= 999
+ */
+class Solution00746 {
+
+  public int minCostClimbingStairs(int[] cost) {
+    int[] dp = new int[cost.length + 1];
+    dp[0] = 0;
+    dp[1] = 0;
+    for (int i = 2; i <= cost.length; i++) {
+      dp[i] = Math.min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2]);
+    }
+    return dp[cost.length];
+  }
+
+  public static void test_00746() {
+    Solution00746 solution = new Solution00746();
+    int[] cost = { 10, 15, 20 };
+    int expected = 15;
+    System.out.println("Expected: " + expected + ", Output: " + solution.minCostClimbingStairs(cost));
+    cost = new int[] { 1, 100, 1, 1, 1, 100, 1, 1, 100, 1 };
+    expected = 6;
+    System.out.println("Expected: " + expected + ", Output: " + solution.minCostClimbingStairs(cost));
+
+  }
+
+  public static void main(String[] args) {
+    test_00746();
+  }
+}
